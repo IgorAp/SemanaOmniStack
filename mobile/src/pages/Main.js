76f593768e16d942adf5bacb703eea495fe59d46
igor silva <igoraparecido59@outlook.com>
2020-02-01 21:21:53 -0300

    init mobile project

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+
+function Main (){
+    return <MapView style={{ flex: 1 }}></MapView>
+}
+
+export default Main;
